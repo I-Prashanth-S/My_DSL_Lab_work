42bd3b5d7d63b5ec38e34e0f89cd886ed8124f16
Prashanth <prashanth.s.edu@gmail.com>
2023-07-30 20:37:34 -0400

    client to work on multiple servers

--- client.py
+++ client.py
@@ -7,7 +7,6 @@ import time
 import numpy as np
 import sys
 
-
 SECRET = b"this is a very secret secret"
 NUM_TRIALS = 1000
 
@@ -15,19 +14,23 @@ client_id = "0000".encode("UTF-8")
 data = "".join([random.choice("ABCDEF") for _ in range(64)]).encode("UTF-8")
 msg_hmac = hmac.new(SECRET, data, digestmod="sha256").digest()
 
+# Replace this with the IP addresses of the servers where the nodes are running
+server_ips = ["ip1", "ip2", "ip3", ...]
+
 start_port = int(sys.argv[1])
 stop_port = int(sys.argv[2])
 num_faulty_nodes = int(sys.argv[3])
 
+
 with open("clusterlist.py", "rb") as fp:   #Pickling
     clusters = pickle.load(fp)
 
-#print(clusters)
 durations = []
 for i in tqdm(range(NUM_TRIALS)):
     start = time.time()
     all_sockets = []
 
+    # Replace random.choice(clusters) with a list of IP addresses for each cluster
     all_ports = list(random.choice(clusters))
 
     primary = random.choice(all_ports)
@@ -37,7 +40,7 @@ for i in tqdm(range(NUM_TRIALS)):
     to_send = code + client_id + msg_hmac + data
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("localhost", primary))
+    s.connect((primary, start_port))  # Use the server IP address instead of "localhost"
     s.sendall(code + client_id + msg_hmac + data)
     all_sockets.append(s)
 
@@ -45,21 +48,16 @@ for i in tqdm(range(NUM_TRIALS)):
     to_send = code + client_id + msg_hmac + data
     for p in all_ports:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(("localhost", p))
+        s.connect((p, start_port))  # Use the server IP address instead of "localhost"
         s.sendall(code + client_id + msg_hmac + data)
         all_sockets.append(s)
 
     for s in all_sockets:
         assert s.recv(4).decode("UTF-8") == "COMM"
         s.close()
-    
+
     duration = time.time() - start
     durations.append(duration)
 
     for s in all_sockets:
         s.close()
-
-
-print("P50:", 1 / np.quantile(durations, 0.5), "tx/s")
-print("P90:", 1 / np.quantile(durations, 0.9), "tx/s")
-print("Mean:", 1 / np.mean(durations), "tx/s")
